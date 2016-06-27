3587c48b3859095e492baccc7c4f9171315547ed
Omar Marquez <Omar.Marquez@imc-chicago.com>
2016-06-27 14:34:24 -0500

    Version as ENV var

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM    alpine:latest
 RUN     apk --no-cache add --update unzip wget gcc g++ make fastjar gcc-java
 ADD     build_pdftk.sh /bin/
+ENV     VER_PDFTK=2.02
 CMD     ["/bin/build_pdftk.sh"]
--- build_pdftk.sh
+++ build_pdftk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
     
-    wget --no-check-certificate http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk-2.02-src.zip && \
-    unzip pdftk-2.02-src.zip && \
-    (cd pdftk-2.02-dist/pdftk && make -f Makefile.Redhat && make -f Makefile.Redhat install) && \
-    rm -rf pdftk-2.02-dist pdftk-2.02-src.zip 
+    wget --no-check-certificate http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk-${VER_PDFTK}-src.zip && \
+    unzip pdftk-${VER_PDFTK}-src.zip && \
+    (cd pdftk-${VER_PDFTK}-dist/pdftk && make -f Makefile.Redhat && make -f Makefile.Redhat install) && \
+    rm -rf pdftk-${VER_PDFTK}-dist pdftk-${VER_PDFTK}-src.zip 
